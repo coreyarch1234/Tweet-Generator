c40f619f3cbe8f9300c06816f73229e6cb15aefa
coreyarch1234 <harrilal@mit.edu>
2017-01-26 15:17:41 -0800

    added a function in histogram

--- histogram/histogram.py
+++ histogram/histogram.py
@@ -1,7 +1,7 @@
+#Store all of the words into an array called, alg_word_set
 alg = open('words_alg.txt', 'r')
 alg_text = alg.readlines()
 alg_word_set = []
 for line in alg_text:
     alg_word_set.extend(line.split())
-
 print(alg_word_set)
