ac9978537a4069e13cfc53c7f0fc6d66a2dd58ba
coreyarch1234 <harrilal@mit.edu>
2017-01-26 21:51:11 -0800

    created histogram

--- histogram/histogram.py
+++ histogram/histogram.py
@@ -4,4 +4,29 @@ alg_text = alg.readlines()
 alg_word_set = []
 for line in alg_text:
     alg_word_set.extend(line.split())
-print(alg_word_set)
+
+def give_default_value():
+    word_dict = {}
+    for key_word in alg_word_set:
+        word_dict[key_word] = 0
+    return word_dict
+
+def histogram():
+    new_word_dict = give_default_value()
+    for key_word in alg_word_set:
+        new_word_dict[key_word] += 1
+    return new_word_dict
+
+def unique_words(histogram_input):
+    unique_total_count = 0
+    for key,value in histogram_input.items():
+        # print("Key : {0}, Value : {1}".format(key, value))
+        if value == 1:
+            # print("HOORAYYYYYYY")
+            unique_total_count += 1
+    return unique_total_count
+
+
+if __name__ == '__main__':
+    histogram()
+    print(unique_words(histogram()))
