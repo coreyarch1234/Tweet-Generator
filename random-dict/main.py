f48f228d642298037f13fcb57bf4ca13eed70bba
coreyarch1234 <harrilal@mit.edu>
2017-01-31 16:07:14 -0800

    modified random dict

--- random-dict/main.py
+++ random-dict/main.py
@@ -2,16 +2,12 @@
 #a number of words, n,  takes a random set and outputs that set in a sentence (grammar/sentence structure does not matter)
 
 #Handles reading text file
-words = open('words.txt', 'r')
-word_set = words.readlines()
-
-word_set_list = []
-# for line in word_set:
-#     word_set_list.append(line.strip())
-
-for i in word_set:
-    thisline = i.split(" ")
-words.close()
+def read_in(word_text):
+    words = open(word_text, 'r')
+    word_list = words.readlines()
+    for i in word_list:
+        thisline = i.split(" ")
+    return thisline
 
 #Handles reading input from user
 num_count = int(raw_input())
@@ -19,10 +15,12 @@ num_count = int(raw_input())
 import random
 import sys
 
-def make_list():
-    for word in thisline:
-        word_set_list.append(word)
-    return word_set_list
+def make_list(word_list):
+    this_line = read_in(word_list)
+    word_list = []
+    for word in this_line:
+        word_list.append(word)
+    return word_list
 
 def random_list(a):
     b = []
@@ -33,18 +31,16 @@ def random_list(a):
     return b
 
 
-def random_sequence(num_count):
+def random_sequence(num_count, word_list):
     array = []
     return_array = []
-    random = make_list()
-    # print("The word is " + str(random))
+    random = make_list(word_list)
     for rand in random_list(random):
         array.append(rand)
-    # print(array)
     for count in range(0, num_count):
         return_array.append(array[count])
     print(return_array)
 
 
 if __name__ == '__main__':
-    random_sequence(num_count)
+    random_sequence(num_count, 'words.txt')
