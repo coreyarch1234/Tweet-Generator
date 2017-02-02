0604a2bc21503003bdaeaeed08471c7dbb5487c2
coreyarch1234 <harrilal@mit.edu>
2017-02-02 15:17:24 -0800

    finished stochastic sampling section

--- histogram/stochastic.py
+++ histogram/stochastic.py
@@ -17,22 +17,22 @@ def random_word_weighted(words):
     word_hist = histogram.histogram(text_list) #histogram creates a histogram of the words
     # Order hist descending, sum the weights, subtract weight from random number loop through weights.
     # If random number is less than the weight, then return the word associated with it.
-    print(sorted(word_hist, key=word_hist.__getitem__))
-    sum_weights = 0.0
-    length = len(word_hist)
+    ordered_hist = list(reversed(sorted(word_hist, key=word_hist.__getitem__)))
+    length = len(text_list) + 0.0 #length of source text
 
-    # for key in reverse_hist: # Gets the sum of the weights
-    #     sum_weights += word_hist[key] / length
-    #
-    # return sum_weights
+    print(ordered_hist)
 
-    random_num = random.random() * sum_weights
+    fish_count = 0
+    for num in range(1,10000):
+        random_num = random.random()
+        for weight_key in ordered_hist:
+            random_num -= (word_hist[weight_key])/length #takes random number (0 to 1) and subtracts the weight until the rand num is less than 0
 
-    # for weight_key in reverse_hist:
-    #     random_num -= word_hist[weight_key] #takes random number (0 to 1) and subtracts the weight until the rand num is less than 0
-    #     if random_num < 0: #The chances are higher of weight_key being a larger weight since it is being subtracted from random number
-    #         return weight_key
-
+            if random_num < 0: #The chances are higher of weight_key being a larger weight since it is being subtracted from random number
+                return weight_key
+    #             if weight_key == "fish":
+    #                 fish_count += 1
+    # print("If my algorithm were perfect, the number of fish would be 5000, but my actual value is " + str(fish_count) + " the error is " + str(100 * (fish_count - 5000.0)/5000) + "%")
 
 if __name__ == '__main__':
     print(random_word_weighted('stochastic_random.txt'))
