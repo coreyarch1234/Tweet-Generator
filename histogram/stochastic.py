18e031622d67729617ccb8d38aa45d7f8f7eca6c
coreyarch1234 <harrilal@mit.edu>
2017-02-06 22:04:23 -0800

    created flask web app, proc file and req.txt

--- histogram/stochastic.py
+++ histogram/stochastic.py
@@ -20,19 +20,19 @@ def random_word_weighted(words):
     ordered_hist = list(reversed(sorted(word_hist, key=word_hist.__getitem__)))
     length = len(text_list) + 0.0 #length of source text
 
-    print(ordered_hist)
+    # print(ordered_hist)
 
     fish_count = 0
-    for num in range(1,10000):
-        random_num = random.random()
-        for weight_key in ordered_hist:
-            random_num -= (word_hist[weight_key])/length #takes random number (0 to 1) and subtracts the weight until the rand num is less than 0
+    random_num = random.random()
+    for weight_key in ordered_hist:
+        random_num -= (word_hist[weight_key])/length #takes random number (0 to 1) and subtracts the weight until the rand num is less than 0
 
-            if random_num < 0: #The chances are higher of weight_key being a larger weight since it is being subtracted from random number
-                return weight_key
-    #             if weight_key == "fish":
-    #                 fish_count += 1
-    # print("If my algorithm were perfect, the number of fish would be 5000, but my actual value is " + str(fish_count) + " the error is " + str(100 * (fish_count - 5000.0)/5000) + "%")
+        if random_num < 0: #The chances are higher of weight_key being a larger weight since it is being subtracted from random number
+            return weight_key
+            # if weight_key == "fish":
+            #     fish_count += 1
+
+# print("If my algorithm were perfect, the number of fish would be 5000, but my actual value is " + str(fish_count) + " the error is " + str(100 * (fish_count - 50000.0)/50000) + "%")
 
 if __name__ == '__main__':
-    print(random_word_weighted('stochastic_random.txt'))
+    print random_word_weighted('stochastic_random.txt')
