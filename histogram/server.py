b87252104d1aed9906ba23cdf658cf9566a554e4
coreyarch1234 <harrilal@mit.edu>
2017-02-07 13:03:13 -0800

    refactored stochastic sampling file

--- histogram/server.py
+++ histogram/server.py
@@ -7,4 +7,4 @@ def hello_world():
     return stochastic.random_word_weighted('stochastic_random.txt')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- histogram/stochastic.py
+++ histogram/stochastic.py
@@ -3,36 +3,36 @@ import random
 import sys
 import histogram
 
-# filename = raw_input("Enter name of file without the txt extension: ") + '.txt'
-
-#This method will take in a txt file and print out a random word.
-def random_word(words):
-    text_list = histogram.read_in(words) #read_in creates a list of words from the text
-    word_hist = histogram.histogram(text_list) #histogram creates a histogram of the words
+#Prints out a random word from a histogram with no weights
+def random_word():
     random_num = random.randint(0, len(text_list) - 1)
     return text_list[random_num]
 
-def random_word_weighted(words):
-    text_list = histogram.read_in(words) #read_in creates a list of words from the text
-    word_hist = histogram.histogram(text_list) #histogram creates a histogram of the words
-    # Order hist descending, sum the weights, subtract weight from random number loop through weights.
-    # If random number is less than the weight, then return the word associated with it.
-    ordered_hist = list(reversed(sorted(word_hist, key=word_hist.__getitem__)))
-    length = len(text_list) + 0.0 #length of source text
-
-    # print(ordered_hist)
-
-    fish_count = 0
+#Uses weighted probability to print out a random word
+def random_word_weighted():
+    ordered_hist = list(reversed(sorted(word_hist, key=word_hist.__getitem__)))#Ordered from greatest to least.
+    length = len(text_list) + 0.0
     random_num = random.random()
-    for weight_key in ordered_hist:
-        random_num -= (word_hist[weight_key])/length #takes random number (0 to 1) and subtracts the weight until the rand num is less than 0
-
-        if random_num < 0: #The chances are higher of weight_key being a larger weight since it is being subtracted from random number
+    for weight_key in ordered_hist: #Runs for each word in the histogram and subtracts the fraction of each word from the random number
+        random_num -= (word_hist[weight_key])/length
+        if random_num < 0: #Once the random number is less than 0, return that word. This will happen more often for words with bigger weighted probabilities
             return weight_key
-            # if weight_key == "fish":
-            #     fish_count += 1
 
-# print("If my algorithm were perfect, the number of fish would be 5000, but my actual value is " + str(fish_count) + " the error is " + str(100 * (fish_count - 50000.0)/50000) + "%")
+#Using a test word, this will calculate how many times that word should ideally appear when taking that word randomly with weight, and will compare it with the observed case.
+def percent_error(test_word, run_count):
+    count = 0
+    ideal_num = run_count * (word_hist[test_word])/(len(text_list) + 0.0)
+    for num in range(1, run_count):
+        if random_word_weighted() == test_word:
+            count += 1
+    print("My percent error after running " + str(run_count) + " times is " + str(100 * (count - ideal_num)/ideal_num) + "%")
 
 if __name__ == '__main__':
-    print random_word_weighted('stochastic_random.txt')
+    #Takes in any text file and creates a histogram named word_hist.
+    filename = raw_input("Enter name of file without the txt extension: ") + '.txt'
+    text_list = histogram.read_in(filename)
+    word_hist = histogram.histogram(text_list)
+    #Prints out the random word using weighted probability
+    print random_word_weighted()
+    #Uses the printed word to calculate the percent error
+    percent_error("fish", 100000)
