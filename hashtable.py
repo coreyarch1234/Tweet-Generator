aafa8c6ff149d615da812dcfe3bf4b92f21586c3
coreyarch1234 <harrilal@mit.edu>
2017-03-16 15:03:17 -0700

    reduced bucket size

--- hashtable.py
+++ hashtable.py
@@ -5,7 +5,7 @@ import random
 
 class HashTable(object):
 
-    def __init__(self, init_size=200000):
+    def __init__(self, init_size=2000):
         """Initialize this hash table with the given initial size"""
         self.buckets = [LinkedList() for i in range(init_size)]
 
