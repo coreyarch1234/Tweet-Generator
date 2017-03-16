5565dcb57dc0b4c7882246b3b95906d919631724
coreyarch1234 <harrilal@mit.edu>
2017-03-15 21:48:41 -0700

    test

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello"
+    histogram = Hashtogram()
+    print(histogram.generate_random_sentence(3))
 
 if __name__ == '__main__':
     app.run()
